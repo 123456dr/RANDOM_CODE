2390df20bbd0a4b0ca9856ff331053cc9dcea1d3
RC <125175307+123456dr@users.noreply.github.com>
2026-05-23 19:11:52 +0800

    9

---
+++ Code/LeetCode/9.cpp
@@ -0,0 +1,12 @@
+// 9. Palindrome Number
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+        int len = s.length();
+        for(int i=0;i<len/2;i++){
+            if(s[i] != s[len-i-1])return false;
+        }
+        return true;
+    }
+};
